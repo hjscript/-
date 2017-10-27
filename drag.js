3b967b8f7da66701c1e8c2ab48c11d76f49af20b
hjscript <651721663@qq.com>
2017-10-27 19:56:11 +0800

    commit

--- drag.js
+++ drag.js
@@ -1,9 +1,9 @@
 function drag(id) {
-	var obj =document.getElementById(id);
-	var disX=0;
-	var disY=0;
-	obj.onmousedown =function(e) {
-		disX=e.pageX-obj.offsetLeft;
-		disY=e.pageY-obj.offsetTop;
-	}
+    var obj = document.getElementById(id);
+    var disX = 0;
+    var disY = 0;
+    obj.onmousedown = function(e) {
+        disX = e.pageX - obj.offsetLeft;
+        disY = e.pageX - obj.offsetTop;
+    }
 }
