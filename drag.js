e20f50affcdfb447283f365ab5e8a7606e6ef123
hjscript <651721663@qq.com>
2017-10-27 18:15:43 +0800

    [update]修改代码

--- drag.js
+++ drag.js
@@ -2,7 +2,8 @@ function drag(id) {
 	var obj =document.getElementById(id);
 	var disX=0;
 	var disY=0;
-	obj.onmousedown =function() {
-		
+	obj.onmousedown =function(e) {
+		disX=e.pageX-obj.offsetLeft;
+		disY=e.pageY-obj.offsetTop;
 	}
 }
